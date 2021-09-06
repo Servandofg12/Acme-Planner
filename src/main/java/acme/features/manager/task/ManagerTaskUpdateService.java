6658e8139bcee5cd9533b72648a427ce68532317
Jesús Roldán Cadena <72762597+jesrolcad@users.noreply.github.com>
2021-09-06 20:19:55 +0200

    Update ManagerTaskUpdateService.java

--- src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
+++ src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
@@ -114,8 +114,11 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 		}
 		//Validacion workload
 		final Double workload = entity.getWorkload();
+		final BigDecimal bd = new BigDecimal(String.valueOf(workload));
+		final BigDecimal fPart = bd.remainder(BigDecimal.ONE);
+		final BigDecimal minutosMax = new BigDecimal(String.valueOf(0.59));
 		final Integer workloadInt = workload.intValue();
-		final Integer workloadDouble = (int) (workload%1.*100);
+		//final Integer workloadDouble = (int) (workload%1.*100);
 		final Boolean workloadCorrecto;
 		if(workload == null || endMoment == null || startMoment == null) {
 			
@@ -124,7 +127,7 @@ public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, 
 	} 
 			else if(workload > 99.59) {
 			errors.state(request, false, "workload","manager.task.error.workloadMax");
-	} else if(workloadDouble >59) {
+	} else if(fPart.compareTo(minutosMax) == 1) {
 		errors.state(request, false, "workload","manager.task.error.workloadMaxMinutes");
 	} else if(workloadInt > 99) {
 		errors.state(request, false, "workload","manager.task.error.workloadMaxHours");
